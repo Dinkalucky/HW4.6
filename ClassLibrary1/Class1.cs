79fe45264588bbe4c715255b047766585fa19fea
Dina Nevhod <Dina_Nevhod@epam.com>
2023-04-08 17:19:31 +0200

    Task 6.5

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public abstract class Class
+    {
+        string classString;
+        public string ClassProperty { get; set; }
+        public abstract void Method();
+    }
+    public class Class1:Class
+    {
+        public string Class1Property { get; set; }
+        public override void Method()
+        {
+            Console.WriteLine("Class1");
+        }
+    }
+    public class Class2 : Class
+    {
+        private string Class2Property { get; set; }
+        public override void Method()
+        {
+            Console.WriteLine("Class2");
+        }
+    }
+}
---
+++ Task6.5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6._5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = null;
+            try
+            {
+                assembly = Assembly.LoadFile("C:\\Users\\Dina_Nevhod\\source\\repos\\HW4.6\\ClassLibrary1\\bin\\Debug\\ClassLibrary1.dll"); //не знала, як обрати файл dll без абсолютного шляху
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("СПИСОК ВСЕХ ТИПОВ В СБОРКЕ: " + assembly.FullName + Environment.NewLine);
+
+            Type[] types = assembly.GetTypes();
+
+            foreach (Type type in types)
+            {
+                Console.WriteLine("Тип:  " + type);
+                var methods = type.GetMethods();
+                if (methods != null)
+                {
+                    foreach (var method in methods)
+                    {
+                        string methStr = "Метод:" + method.Name + "\n";
+                        var methodBody = method.GetMethodBody();
+                        if (methodBody != null)
+                        {
+                            var byteArray = methodBody.GetILAsByteArray();
+
+                            foreach (var b in byteArray)
+                            {
+                                methStr += b + ":";
+                            }
+                        }
+
+                        Console.WriteLine(methStr);
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
